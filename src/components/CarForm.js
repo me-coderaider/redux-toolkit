06c84357500fdee511d6b9b779e9cad15c0cee65
me-coderaider <kashyappankajnsp@gmail.com>
2024-11-09 11:28:41 +0530

    dispatching 'addCar()' and displaying 'cars' details by using data from 'redux-store'

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,21 +1,43 @@
-import { useDispatch, useSelector } from "react-redux";
-import { changeName } from "../store";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+// using 'shallowEqual' to avoid warning (Selectors that return a new reference (such as an object or an array) should be memoized:)
+// https://stackoverflow.com/questions/67384049/how-exactly-useselector-works
+import { changeName, changeCost, addCar } from "../store";
 
 function CarForm() {
     const dispatch = useDispatch();
 
-    const name = useSelector((state) => {
-        return state.form.name;
-    });
+    const { name, cost } = useSelector((state) => {
+        return {
+            name: state.form.name,
+            cost: state.form.cost,
+        };
+    }, shallowEqual);
 
     const handleNameChange = (event) => {
         dispatch(changeName(event.target.value));
     };
 
+    const handleCostChange = (event) => {
+        // '|| 0' to deal with scenario if we get 'NaN' from parseInt()
+        const carCost = parseInt(event.target.value) || 0;
+        dispatch(changeCost(carCost));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(
+            addCar({
+                name: name,
+                cost: cost,
+            })
+        );
+    };
+
     return (
         <div className="car-form panel">
             <h4 className="subtitle is-3">Add Car</h4>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="field-group">
                     <div className="field">
                         <label className="label">Name</label>
@@ -25,6 +47,18 @@ function CarForm() {
                             onChange={handleNameChange}
                         />
                     </div>
+                    <div className="field">
+                        <label className="label">Cost</label>
+                        <input
+                            className="input is-expanded"
+                            value={cost || ""}
+                            onChange={handleCostChange}
+                            type="number"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <button className="button is-link">Submit</button>
                 </div>
             </form>
         </div>
--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,37 @@
+import { useSelector } from "react-redux";
+
 function CarList() {
-    return <div>CarList</div>;
+    const cars = useSelector((state) => {
+        // return state.cars.cars; weird, 'cars.cars' because we have defined 'cars' property in the 'cars' slice
+        // So, to avoid the confusion, we'll change the 'cars' property.
+        // return state.cars.cars;
+
+        return state.cars.data;
+    });
+
+    const handleDelete = (car) => {};
+
+    const renderedCars = cars.map((car) => {
+        return (
+            <div key={car.id} className="panel">
+                <p>
+                    {car.name} - ${car.cost}
+                </p>
+                <button
+                    className="button is-danger"
+                    onClick={() => handleDelete(car)}
+                >
+                    Delete
+                </button>
+            </div>
+        );
+    });
+
+    return (
+        <div className="car-list">
+            {renderedCars} <hr />
+        </div>
+    );
 }
 
 export default CarList;
